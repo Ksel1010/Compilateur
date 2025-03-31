eb1147191f81ca0f2b5f8d64ac1f8b6bef2e6f31
sellami <kaissellami1@gmail.com>
2025-03-31 22:46:35 +0200

    remove unuseful files

--- test.c
+++ test.c
@@ -2,10 +2,12 @@ int main()
 {
     int a  = 7;
     int b = 0;
+    int c = 0;
 
-    for (a = 0; a < 5 ; a = a+1 ) {
-        b  =1;
+    while ( a==0){
+        a = 5;
     }
+    
 
-    int c = 9;
+    c = 9;
 }
